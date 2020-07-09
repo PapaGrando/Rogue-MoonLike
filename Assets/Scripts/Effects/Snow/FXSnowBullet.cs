a5410aaf603fa441501e0656825b062d41c49729
PapaGrando <borislovesalo@gmail.com>
2020-07-09 18:08:38 +0300

    #10 логика управления некоторых эффектов

---
+++ Assets/Scripts/Effects/Snow/FXSnowBullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FXSnowBullet : FXActivator
+{
+    private ParticleSystem _particleSystem;
+
+    void Start()
+    {
+        base.Start();
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    public override void EnableFx(bool val)
+    {
+        base.EnableFx(val);
+
+        if (val)
+            _particleSystem.Play();
+        else
+            _particleSystem.Stop();
+    }
+}
---
+++ Assets/Scripts/Effects/Speed/FXSpeedActivator.cs
@@ -0,0 +1,4 @@
+﻿public class FXSpeedActivator : FXActivator
+{
+    protected override void Start() => base.Start();
+}
