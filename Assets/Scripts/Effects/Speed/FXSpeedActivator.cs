ca4cb0565b72d19fe0a50b9b32a31c3480087668
PapaGrando <borislovesalo@gmail.com>
2020-07-09 23:56:22 +0300

    #10 прочие анимации эффектов

--- Assets/Scripts/Effects/Speed/FXSpeedActivator.cs
+++ Assets/Scripts/Effects/Speed/FXSpeedActivator.cs
@@ -1,4 +1,30 @@
-﻿public class FXSpeedActivator : FXActivator
+﻿using UnityEngine;
+
+//todo : рефактор
+
+public class FXSpeedActivator : FXActivator { }
+
+public class FXImpactActivator : FXActivator
 {
-    protected override void Start() => base.Start();
+    [SerializeField] private bool _randomize = false;
+    [Range(0, 3)] [SerializeField] private int _type = 0;
+
+    protected override void Start()
+    {
+        _type = _randomize ? RandomizeType() : _type;
+        Animator.SetInteger("Type", _type);
+        base.Start();
+    }
+
+    public void EnableFx(bool val, int type)
+    {
+        EnableFx(val);
+
+        if (!val) return;
+
+        Animator.SetInteger("Type", _type);
+        Animator.Play("Play");
+    }
+
+    public static int RandomizeType() => Random.Range(0, 4);
 }
