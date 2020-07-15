fae6b6565f7eb5c7d73a692906ca1c60f6add574
PapaGrando <borislovesalo@gmail.com>
2020-07-16 01:12:42 +0300

    #10 модели и контроллеры мобов и анимаций\

---
+++ Assets/Scripts/Models/MobAnimationStates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+[Serializable]
+public class MobAnimationStates
+{
+    public bool IsIdle;
+    public bool IsDamaging;
+    public bool IsDead;
+    public bool IsRunning;
+    public bool IsJumping;
+    public bool IsFalling;
+    public bool IsAttacking;
+    public bool OnSpecialState;
+    public bool IsSpecialMove;
+    public bool IsSpecialAttacking;
+
+    public MobAnimationStates()
+    {
+        IsIdle = false;
+        IsDamaging = false;
+        IsDead = false;
+        IsRunning = false;
+        IsJumping = false;
+        IsFalling = false;
+        IsAttacking = false;
+        OnSpecialState = false;
+        IsSpecialMove = false;
+        IsSpecialAttacking = false;
+    }
+}
---
+++ Assets/Scripts/Models/MobStats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+[Serializable]
+public class MobStats
+{
+    public int Health;
+    public int Attack;
+    public int Defense;
+
+    public int Strength;
+    public int Agility;
+    public int Intelligence;
+
+    public MobStats()
+    {
+        Health = 1;
+        Attack = 1;
+        Defense = 1;
+
+        Strength = 1;
+        Agility = 1;
+        Intelligence = 1;
+    }
+}
