cdbe6812f58398f4f93829be1ea8d9caba073096
PapaGrando <borislovesalo@gmail.com>
2020-08-19 19:43:40 +0300

    #12 контроллеры тача

---
+++ Assets/Scripts/Enums/TouchPadMode.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+///     Режимы работы тачпада
+///     <para>LeftRightButtons - Пространство разделено на 2 кнопки (движение вправо и влево)</para>
+///     <para>MoveToTouchedPos - Персонаж передвигается в позицию указанную косанием</para>
+/// </summary>
+public enum TouchPadMode
+{
+    Disable,
+    LeftRightButtons,
+    MoveToTouchedPos
+}
