f1391c311ba8ff99eb98e6b78bac2b459e831626
PapaGrando <borislovesalo@gmail.com>
2020-08-19 19:42:13 +0300

    #12 UnityEvent Generics

---
+++ Assets/Scripts/GenericUnityEvents.cs
@@ -0,0 +1,14 @@
+﻿//Тут кастомные UnityEvent, которые возвращают значения
+//todo : сделать на основе делегатов и удалить это
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEventVector2 : UnityEvent<Vector2> { }
+public class UnityEventBool : UnityEvent<bool> { }
+/// <summary>
+/// Событие, которое возвращает структуру словаря, где Key - бафф, Value - время действия
+/// </summary>
+public class UnityEventBuffsDictionary : UnityEvent<Dictionary<BuffStats, float>> {}
+public class UnityEventMobStats : UnityEvent<MobStats> { }
