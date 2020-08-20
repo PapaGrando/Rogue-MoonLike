7ea6958a130236988d826803ab90f45bcfd81c68
PapaGrando <borislovesalo@gmail.com>
2020-08-20 23:22:13 +0300

    #14 менеджер для загрузки синглтонов и других менеджеров

---
+++ Assets/Scripts/LoadingScreen/LoadingManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _loadingPanel;
+    [SerializeField] private string _nextScene;
+    [SerializeField] public List<MonoBehaviour> _managersToLoad;
+    
+    void Awake()
+    {
+        var panel = Instantiate(_loadingPanel).GetComponent<LoadingPanelController>();
+        panel.SetSceneToLoad(_nextScene);
+
+        var gameObjectWithManagers = Instantiate(new GameObject());
+        gameObjectWithManagers.name = "ManagersContainer";
+        DontDestroyOnLoad(gameObjectWithManagers);
+
+       /*
+        foreach (var manager in _managersToLoad)
+        {
+            gameObjectWithManagers.AddComponent(manager.GetType());
+        }
+       */
+    }
+}
