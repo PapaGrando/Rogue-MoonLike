e7dc8bacb510d030d17c306dab3149eb07f0cc4a
PapaGrando <borislovesalo@gmail.com>
2020-08-27 20:03:57 +0300

    #16 активатор паузы

---
+++ Assets/Scripts/Pause/PauseActivator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PauseActivator : MonoBehaviour
+{
+    [SerializeField] private GameObject _pauseWindowPrefab;
+
+    public void OnButtonPressed()
+    {
+        if (!PauseController.Paused) Instantiate(_pauseWindowPrefab);
+    }
+}
