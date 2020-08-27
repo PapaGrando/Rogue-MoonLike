a2cc89e84c0237a292b3da9cba0d7d000903dd96
PapaGrando <borislovesalo@gmail.com>
2020-08-27 19:47:32 +0300

    #16 контроллер паузы

---
+++ Assets/Scripts/Pause/PauseController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseController : MonoBehaviour
+{
+    public static bool Paused = false;
+
+    [SerializeField] private Slider _soundSlider;
+    [SerializeField] private AudioClip _buttonPressSound;
+
+    private Animator _anim;
+    private Camera _camera;
+
+    void Awake()
+    {
+        if (Paused)
+        {
+            Destroy(gameObject);
+            Debug.LogWarning("Pause already created, new instance will destroyed. Check PauseController.Paused before instancing");
+        }
+
+        _anim = GetComponent<Animator>();
+        _camera = Camera.main;
+        _soundSlider.value = AudioListener.volume;
+
+        Paused = true;
+    }
+
+    public void OnSoundSliderValueChanged()
+    {
+        AudioListener.volume = _soundSlider.value;
+    }
+
+    public void OnResumePressed()
+    {
+        _anim.SetTrigger("Resume");
+        if (_buttonPressSound != null) AudioSource.PlayClipAtPoint(_buttonPressSound, _camera.transform.position, 1f);
+    }
+
+    public void OnQuitPressed()
+    {
+        _anim.SetTrigger("Quit");
+        if (_buttonPressSound != null) AudioSource.PlayClipAtPoint(_buttonPressSound, _camera.transform.position, 1f);
+    }
+
+    public void OnResumeAnimationEnded()
+    {
+        Paused = false;
+        Destroy(gameObject);
+    }
+
+    public void OnQuitAnimationEnded()
+    {
+        Paused = false;
+        Application.Quit();
+    }
+}
